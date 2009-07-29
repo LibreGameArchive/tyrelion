965c0c5f5d68a33dde13c31b6bdec8b78a8ba157
daennart <daennart@2b440a80-7abf-11de-a7be-13e53affbef8>
2009-07-29 22:10:38 +0000

    Credits implemented. Settings-State implemented.

--- trunk/main/src/tyrelion/menu/MenuCredits.java
+++ trunk/main/src/tyrelion/menu/MenuCredits.java
@@ -5,8 +5,13 @@ package tyrelion.menu;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.gui.AbstractComponent;
+import org.newdawn.slick.gui.ComponentListener;
+import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -14,11 +19,18 @@ import org.newdawn.slick.state.StateBasedGame;
  * @author jahudi
  *
  */
-public class MenuCredits extends BasicGameState {
+public class MenuCredits extends BasicGameState implements ComponentListener{
 
 	public static final int ID = 5;
 	
 	private StateBasedGame game;
+	private GameContainer gameContainer;
+	
+	/** Hintergrundgrafik des Hauptmenüs. */
+	private Image background;
+	
+	/** MOA für den Button "zurück" */
+	private MouseOverArea btn_back;
 	
 	/* (non-Javadoc)
 	 * @see org.newdawn.slick.state.BasicGameState#getID()
@@ -33,6 +45,13 @@ public class MenuCredits extends BasicGameState {
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		this.game = game;
+		this.gameContainer = container;
+		
+		gameContainer.setMouseCursor("res/img/mouse/cursor_sword.png", 2, 2);
+		
+		background = new Image("res/img/menu/credits/credits_bg.png");
+	
+		initButtonField();
 
 	}
 
@@ -41,8 +60,10 @@ public class MenuCredits extends BasicGameState {
 	 */
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-		g.drawString("Credits Menu", 50, 50);
-
+		g.clear();
+		g.drawImage(background, 0, 0);
+		
+		renderButtonField(container, g);
 	}
 
 	/* (non-Javadoc)
@@ -58,6 +79,25 @@ public class MenuCredits extends BasicGameState {
 		if (i == Input.KEY_ESCAPE) {
 			game.enterState(MenuMain.ID);
 		}
+	}
+	
+	private void initButtonField() throws SlickException{		
+		//Jeweils Zuweisung von MOAs und Rollover-Images für die Buttons        
+        btn_back = new MouseOverArea(gameContainer, new Image("res/img/menu/main/buttons/mainmenu_button_back_1.png"), 590, 700, 400, 50, this);
+        btn_back.setMouseOverImage(new Image("res/img/menu/main/buttons/mainmenu_button_back_2.png"));
+	}
+	
+	private void renderButtonField(GameContainer container, Graphics g){
+		//Rendern der Buttons
+		btn_back.render(container, g);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.newdawn.slick.gui.ComponentListener#componentActivated(org.newdawn.slick.gui.AbstractComponent)
+	 */
+	public void componentActivated(AbstractComponent source) {
+		//Abfrage des aktivierten Buttons und ausführen der zugehörigen Aktion
+		if (source == btn_back) game.enterState(MenuMain.ID);
 	}
 
 }
