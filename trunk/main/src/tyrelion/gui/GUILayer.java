1de6a57f7cd3eda4c5a2a91a493beb930b82df3e
daennart <daennart@2b440a80-7abf-11de-a7be-13e53affbef8>
2009-08-03 23:52:26 +0000

    GUILayer is a class now

--- trunk/main/src/tyrelion/gui/GUILayer.java
+++ trunk/main/src/tyrelion/gui/GUILayer.java
@@ -21,15 +21,10 @@ import tyrelion.music.MusicManager;
  * @author jahudi, daennart
  *
  */
-public class GUILayer extends BasicGameState  implements ComponentListener{
-
-	public static final int ID = 23;
+public class GUILayer implements ComponentListener{
 	
 	private StateBasedGame game;
 	private GameContainer gameContainer;
-	
-	/** Backgroundimage */
-	private Image background;
 	
 	/** Background for avatar-area */
 	private Image gui_avatar;
@@ -60,71 +55,18 @@ public class GUILayer extends BasicGameState  implements ComponentListener{
 	
 	/** Should the menu be drawn */
 	private boolean isShowMenu = false;
-	
-	/* (non-Javadoc)
-	 * @see org.newdawn.slick.state.BasicGameState#getID()
-	 */
-	@Override
-	public int getID() {
-		
-		return ID;
-		
-	}
 
-	/* (non-Javadoc)
-	 * @see org.newdawn.slick.state.GameState#init(org.newdawn.slick.GameContainer, org.newdawn.slick.state.StateBasedGame)
-	 */
-	public void init(GameContainer container, StateBasedGame game)
+	public GUILayer(GameContainer container)
 			throws SlickException {
 		
-		this.game = game;
 		this.gameContainer = container;	
-		
-		// background = new Image("res/img/menu/main/mainmenu_bg.png");
 		
 		initGUI();
 		
       }
 
-	/* (non-Javadoc)
-	 * @see org.newdawn.slick.state.GameState#render(org.newdawn.slick.GameContainer, org.newdawn.slick.state.StateBasedGame, org.newdawn.slick.Graphics)
-	 */
-	public void render(GameContainer container, StateBasedGame game, Graphics g)
+	public void render(GameContainer container, Graphics g)
 			throws SlickException {
-		
-		g.clear();
-		//g.drawImage(background, 0, 0);
-		renderGUI(container, g);
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see org.newdawn.slick.state.GameState#update(org.newdawn.slick.GameContainer, org.newdawn.slick.state.StateBasedGame, int)
-	 */
-	public void update(GameContainer container, StateBasedGame game, int delta)
-			throws SlickException {
-
-	}
-	
-	private void initGUI() throws SlickException{
-		//Initalisation of the GUI-images
-		gui_avatar = new Image("res/img/gui/gui_avatar.png");
-		gui_menu = new Image("res/img/gui/gui_menu.png");
-		gui_minimap = new Image("res/img/gui/gui_minimap.png");
-		gui_quickslots = new Image("res/img/gui/gui_quickslots.png");
-		gui_infobox = new Image("res/img/gui/gui_infobox.png");
-		
-		//Jeweils Zuweisung von MOAs und Rollover-Images für die Buttons
-		gui_btn_menu = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_menu_1.png"), 808, 525, 29, 27, this);
-		gui_btn_menu.setMouseOverImage(new Image("res/img/gui/gui_btn_menu_2.png"));
-        
-		gui_btn_questlog = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_questlog_1.png"), 380, 672, 195, 96, this);
-		gui_btn_questlog.setMouseOverImage(new Image("res/img/gui/gui_btn_questlog_2.png"));
-		
-		initMenu();
-	}
-	
-	private void renderGUI(GameContainer container, Graphics g) throws SlickException{
 		//Render images
 		g.drawImage(gui_avatar, 10, 0);
 		g.drawImage(gui_quickslots, 450, 699);
@@ -140,14 +82,22 @@ public class GUILayer extends BasicGameState  implements ComponentListener{
 		
 	}
 	
-	private void showMenu(GameContainer container, Graphics g){
-		g.drawImage(gui_menu, 819, 150);
+	private void initGUI() throws SlickException{
+		//Initalisation of the GUI-images
+		gui_avatar = new Image("res/img/gui/gui_avatar.png");
+		gui_menu = new Image("res/img/gui/gui_menu.png");
+		gui_minimap = new Image("res/img/gui/gui_minimap.png");
+		gui_quickslots = new Image("res/img/gui/gui_quickslots.png");
+		gui_infobox = new Image("res/img/gui/gui_infobox.png");
 		
-		gui_btn_back.render(container, g);
-		gui_btn_save.render(container, g);
-		gui_btn_load.render(container, g);
-		gui_btn_settings.render(container, g);
-		gui_btn_quit.render(container, g);
+		//button initialisation
+		gui_btn_menu = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_menu_1.png"), 808, 525, 29, 27, this);
+		gui_btn_menu.setMouseOverImage(new Image("res/img/gui/gui_btn_menu_2.png"));
+        
+		gui_btn_questlog = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_questlog_1.png"), 380, 672, 195, 96, this);
+		gui_btn_questlog.setMouseOverImage(new Image("res/img/gui/gui_btn_questlog_2.png"));
+		
+		initMenu();
 	}
 	
 	private void initMenu() throws SlickException{
@@ -166,6 +116,16 @@ public class GUILayer extends BasicGameState  implements ComponentListener{
 		
 		gui_btn_quit = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_quit_1.png"), 855, 320, 160, 30, this);
 		gui_btn_quit.setMouseOverImage(new Image("res/img/gui/gui_btn_quit_2.png"));
+	}
+	
+	private void showMenu(GameContainer container, Graphics g){
+		g.drawImage(gui_menu, 819, 150);
+		
+		gui_btn_back.render(container, g);
+		gui_btn_save.render(container, g);
+		gui_btn_load.render(container, g);
+		gui_btn_settings.render(container, g);
+		gui_btn_quit.render(container, g);
 	}
 
 	/* (non-Javadoc)
