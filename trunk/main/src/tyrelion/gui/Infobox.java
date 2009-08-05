a7eab75307abea71ef446736a03ffb9db06697f4
daennart <daennart@2b440a80-7abf-11de-a7be-13e53affbef8>
2009-08-05 22:32:18 +0000

    Infobox almost implemented

--- trunk/main/src/tyrelion/gui/Infobox.java
+++ trunk/main/src/tyrelion/gui/Infobox.java
@@ -19,10 +19,6 @@ import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.StateBasedGame;
 
 import tyrelion.FontManager;
-import tyrelion.menu.MenuCredits;
-import tyrelion.menu.MenuLoad;
-import tyrelion.menu.MenuMain;
-import tyrelion.menu.MenuSettings;
 
 /**
  * @author daennart
@@ -37,13 +33,13 @@ public class Infobox implements ComponentListener{
 	private UnicodeFont font;
 	
 	/** Collection of Messages */
-	private ArrayList<String> messages;
+	private ArrayList<Message> messages;
 	
 	/** Lines visible in the box */
-	private int visibleLineCount = 2;
+	private int visibleLineCount = 6;
 	
 	/** Mximum length of one line */
-	private int maxLength = 60;
+	private int maxLength = 30;
 	
 	/** First line to be rendered */
 	private int startLine = 0 ;
@@ -71,6 +67,8 @@ public class Infobox implements ComponentListener{
 		
 		background = new Image("res/img/gui/gui_infobox.png");
 		
+		messages = new ArrayList<Message>();
+		
       }
 
 	public void render(GameContainer container, Graphics g, int x, int y)
@@ -81,23 +79,86 @@ public class Infobox implements ComponentListener{
 		//Render images
 		g.drawImage(background, posX, posY);
 		
-		for (int i=startLine;i==5;i++){
-			font.drawString(posX+20, posY+20*i, messages.get(i));
+		ArrayList<Message> output = transform(messages);
+		
+		for (int i=visibleLineCount;i>startLine;i--){
+			
+			if (output.size()>=i) {
+				
+				font.getEffects().clear();
+				
+				switch (output.get(i-1).getCategory()){
+				case 1:		// Systemmessages
+					font.getEffects().add(new ColorEffect(Color.YELLOW));
+					break;
+				case 2:		// Questmessages
+					font.getEffects().add(new ColorEffect(Color.DARK_GRAY));
+					break;
+				case 3:		// Fightmessages
+					font.getEffects().add(new ColorEffect(Color.RED));
+					break;
+				case 4:		// Itemmessages
+					font.getEffects().add(new ColorEffect(Color.WHITE));
+					break;
+				case 5:		// Experiencemessages
+					font.getEffects().add(new ColorEffect(Color.BLUE));
+					break;
+				case 6:		// Misc. messages
+					font.getEffects().add(new ColorEffect(Color.BLACK));
+					break;
+				default:	// everything else
+					font.getEffects().add(new ColorEffect(Color.MAGENTA));
+				}
+				
+				font.drawString(posX+20, posY+15*(i), output.get(i-1).getText());
+				
+			}
+				
 		}
 		
 	}
 	
-	public void print(String text){
-		messages.add(text);
+	public void print(String text, int category){
+		messages.add(new Message(text, category));
 	}
 	
-	private ArrayList<String> transform(ArrayList<String> messages){
+	public void print(Message message){
+		messages.add(message);
+	}
+	
+	private ArrayList<Message> transform(ArrayList<Message> messages){
 		
-		ArrayList<String> transformedMessages = new ArrayList<String>();
+		ArrayList<Message> transformedMessages = new ArrayList<Message>();
 		
+		String tempString;
 		
+		for (Message elem : messages) {
+			if (elem.getText().length()<maxLength) {
+				transformedMessages.add(elem);
+			} else {
+				tempString = elem.getText();
+				while (tempString.length()>0) {
+					if (tempString.length()<maxLength) {
+						transformedMessages.add(new Message(tempString, elem.getCategory()));
+						tempString = "";
+					} else {
+						transformedMessages.add(new Message(tempString.substring(0, maxLength), elem.getCategory()));
+						tempString = tempString.substring(maxLength+1);
+					}
+				}
+			}
+				
+		}
 		
 		return transformedMessages;
+	}
+	
+	private void scrollUp(){
+		if (startLine<transform(messages).size()) startLine++;
+	}
+	
+	private void scrollDown(){
+		if (startLine>0) startLine--;
 	}
 
 	/* (non-Javadoc)
@@ -105,8 +166,8 @@ public class Infobox implements ComponentListener{
 	 */
 	public void componentActivated(AbstractComponent source) {
 		//Abfrage des aktivierten Buttons und ausführen der zugehörigen Aktion
-		if (source == gui_btn_up) game.enterState(MenuMain.ID);
-		if (source == gui_btn_down) game.enterState(MenuMain.ID);
+		if (source == gui_btn_up) scrollUp();
+		if (source == gui_btn_down) scrollDown();
 	}
 
 }
