46f94c67f26461ad03d2ab2c8d4cc177a8e7e3ff
jahudi84 <jahudi84@2b440a80-7abf-11de-a7be-13e53affbef8>
2009-08-06 18:13:56 +0000

    created stripe1 tag

---
+++ tags/stripe1/src/tyrelion/FontManager.java
@@ -0,0 +1,131 @@
+/**
+ * 
+ */
+package tyrelion;
+
+
+
+import java.util.HashMap;
+
+import org.newdawn.slick.AngelCodeFont;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Font;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+/**
+ * @author imladriel
+ *
+ */
+public class FontManager{
+	
+	public static int FANCY = 1;
+	
+	public static int SIMPLE = 2;
+	
+	public static float SMALL = 1;
+	public static float MEDIUM = 2;
+	public static float LARGE = 3;
+	
+	private int defaultStyle = SIMPLE;
+	
+	private Color defaultColor = Color.black;
+	
+	private float defaultSize = MEDIUM;
+	
+	private static FontManager instance = null;
+	
+	private HashMap<Integer, AngelCodeFont> fancy_fonts;
+	private HashMap<Integer, AngelCodeFont> simple_fonts;
+	
+	public FontManager() throws SlickException{
+		
+		fancy_fonts = new HashMap<Integer, AngelCodeFont>();
+		simple_fonts = new HashMap<Integer, AngelCodeFont>();
+		initFonts();
+	}
+	
+	public static FontManager getInstance() throws SlickException{
+		if (instance == null) {
+			instance = new FontManager();
+		}
+		return instance;
+	}
+	
+	private void initFonts() throws SlickException{
+		fancy_fonts.put(1, new AngelCodeFont("res/fonts/fancy18.fnt","res/fonts/fancy18.png"));
+		fancy_fonts.put(2, new AngelCodeFont("res/fonts/fancy24.fnt","res/fonts/fancy24.png"));
+		fancy_fonts.put(3, new AngelCodeFont("res/fonts/fancy36.fnt","res/fonts/fancy36.png"));
+		
+		simple_fonts.put(1, new AngelCodeFont("res/fonts/simple11.fnt","res/fonts/simple11_0.png"));
+		simple_fonts.put(2, new AngelCodeFont("res/fonts/simple14.fnt","res/fonts/simple14_0.png"));
+		simple_fonts.put(3, new AngelCodeFont("res/fonts/simple18.fnt","res/fonts/simple18_0.png"));
+	}
+	
+	/** Draws String. */
+	public void drawString(Graphics g, float x, float y, String text, int style, float size, Color color){
+		Font oldFont = g.getFont();
+		Color oldColor = g.getColor();
+		
+		Font newFont = chooseFonts(style).get((int) size);
+		
+		g.setFont(newFont);
+		g.setColor(color);
+		
+		g.drawString(text, x, y);
+		
+		g.setFont(oldFont);
+		g.setColor(oldColor);
+	}
+	
+	public void drawString(Graphics g, float x, float y, String text, int style, float size){
+		drawString(g, x, y, text, style, size, defaultColor);
+	}
+	
+	public void drawString(Graphics g, float x, float y, String text, int style, Color color){
+		drawString(g, x, y, text, style, defaultStyle, color);
+	}
+	
+	public void drawString(Graphics g, float x, float y, String text, float size, Color color){
+		drawString(g, x, y, text, defaultStyle, size, color);
+	}
+	
+	public void drawString(Graphics g, float x, float y, String text, int style){
+		drawString(g, x, y, text, style, defaultSize, defaultColor);
+	}
+	
+	public void drawString(Graphics g, float x, float y, String text, float size){
+		drawString(g, x, y, text, defaultStyle, size, defaultColor);
+	}
+	
+	public void drawString(Graphics g, float x, float y, String text, Color color){
+		drawString(g, x, y, text, defaultStyle, defaultSize, color);
+	}
+	
+	public void drawString(Graphics g, float x, float y, String text){
+		drawString(g, x, y, text, defaultStyle, defaultSize, defaultColor);
+	}
+	
+	/** Returns requested TrueTypeFont.*/
+	public Font getFont(int style, float size){
+		return chooseFonts(style).get(size);
+	}
+	
+	/** Adds a font to the font.database */
+	public void addFont(int style, AngelCodeFont font, int size){
+		chooseFonts(style).put(size, font);
+	}
+	
+	/** Chooses requested fontmap.*/
+	private HashMap<Integer, AngelCodeFont> chooseFonts(int style){
+		switch (style){
+		case 1:
+			return fancy_fonts;
+		case 2:
+			return simple_fonts;
+		default:
+			return simple_fonts;
+		}
+	}
+	
+}
---
+++ tags/stripe1/src/tyrelion/gui/GUILayer.java
@@ -0,0 +1,160 @@
+/**
+ * 
+ */
+package tyrelion.gui;
+
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.gui.AbstractComponent;
+import org.newdawn.slick.gui.ComponentListener;
+import org.newdawn.slick.gui.MouseOverArea;
+import org.newdawn.slick.state.StateBasedGame;
+
+import tyrelion.FontManager;
+import tyrelion.ExpMode;
+import tyrelion.menu.MenuCredits;
+import tyrelion.menu.MenuLoad;
+import tyrelion.menu.MenuMain;
+import tyrelion.menu.MenuSettings;
+
+/**
+ * @author jahudi, daennart
+ *
+ */
+public class GUILayer implements ComponentListener{
+	
+	private StateBasedGame game;
+	private GameContainer gameContainer;	
+	
+	/** Background for avatar-area */
+	private Image gui_avatar;
+	/** Background for avatar-area */
+	private Image gui_menu;
+	/** Background for avatar-area */
+	private Image gui_minimap;
+	/** Background for avatar-area */
+	private Image gui_quickslots;
+	
+	/** MOA for ingame-menu-button */
+	private MouseOverArea gui_btn_menu;
+	/** MOA for questlog-button */
+	private MouseOverArea gui_btn_questlog;
+	
+	/** MOA for ingame-menu-button */
+	private MouseOverArea gui_btn_back;
+	/** MOA for ingame-menu-button */
+	private MouseOverArea gui_btn_save;
+	/** MOA for ingame-menu-button */
+	private MouseOverArea gui_btn_load;
+	/** MOA for ingame-menu-button */
+	private MouseOverArea gui_btn_settings;
+	/** MOA for ingame-menu-button */
+	private MouseOverArea gui_btn_quit;
+	
+	/** Should the menu be drawn */
+	private boolean isShowMenu = false;
+
+	public GUILayer(GameContainer container, StateBasedGame game)
+			throws SlickException {
+		
+		this.game = game;
+		this.gameContainer = container;	
+		
+	//	font_head = new UnicodeFont("/res/fonts/vinque.ttf", 48, false, false);
+	//	font_head.getEffects().add(new ColorEffect(new Color(0x00461800)));
+		
+		initGUI();
+		
+      }
+
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		//Render images
+		g.drawImage(gui_avatar, 10, 0);
+		g.drawImage(gui_quickslots, 578, 795);
+		g.drawImage(gui_minimap, 853, 608);
+		
+		//Render buttons
+		gui_btn_menu.render(container, g);
+		gui_btn_questlog.render(container, g);
+		
+		//Display location
+		FontManager.getInstance().drawString(g, 945, 650, "Arthlet", FontManager.FANCY, FontManager.LARGE, new Color(0x00762900));
+		
+		//Check if menu should be drawn and render it
+		if (isShowMenu) showMenu(container, g);
+		
+	}
+	
+	private void initGUI() throws SlickException{
+		//Initalisation of the GUI-images
+		gui_avatar = new Image("res/img/gui/gui_avatar.png");
+		gui_menu = new Image("res/img/gui/gui_menu.png");
+		gui_minimap = new Image("res/img/gui/gui_minimap.png");
+		gui_quickslots = new Image("res/img/gui/gui_quickslots.png");
+		
+		
+		//button initialisation
+		gui_btn_menu = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_menu_1.png"), 936, 621, 29, 27, this);
+		gui_btn_menu.setMouseOverImage(new Image("res/img/gui/gui_btn_menu_2.png"));
+        
+		gui_btn_questlog = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_questlog_1.png"), 508, 768, 195, 96, this);
+		gui_btn_questlog.setMouseOverImage(new Image("res/img/gui/gui_btn_questlog_2.png"));
+		
+		initMenu();
+	}
+	
+	private void initMenu() throws SlickException{
+		//Jeweils Zuweisung von MOAs und Rollover-Images für die Buttons
+		gui_btn_back = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_back_1.png"), 983, 296, 160, 30, this);
+		gui_btn_back.setMouseOverImage(new Image("res/img/gui/gui_btn_back_2.png"));
+		
+		gui_btn_save = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_save_1.png"), 983, 326, 160, 30, this);
+		gui_btn_save.setMouseOverImage(new Image("res/img/gui/gui_btn_save_2.png"));
+        
+		gui_btn_load = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_load_1.png"), 983, 356, 160, 30, this);
+		gui_btn_load.setMouseOverImage(new Image("res/img/gui/gui_btn_load_2.png"));
+		
+		gui_btn_settings = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_settings_1.png"), 983, 386, 160, 30, this);
+		gui_btn_settings.setMouseOverImage(new Image("res/img/gui/gui_btn_settings_2.png"));
+		
+		gui_btn_quit = new MouseOverArea(gameContainer, new Image("res/img/gui/gui_btn_quit_1.png"), 983, 416, 160, 30, this);
+		gui_btn_quit.setMouseOverImage(new Image("res/img/gui/gui_btn_quit_2.png"));
+	}
+	
+	private void showMenu(GameContainer container, Graphics g){
+		g.drawImage(gui_menu, 947, 246);
+		
+		gui_btn_back.render(container, g);
+		gui_btn_save.render(container, g);
+		gui_btn_load.render(container, g);
+		gui_btn_settings.render(container, g);
+		gui_btn_quit.render(container, g);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.newdawn.slick.gui.ComponentListener#componentActivated(org.newdawn.slick.gui.AbstractComponent)
+	 */
+	public void componentActivated(AbstractComponent source) {
+		//Abfrage des aktivierten Buttons und ausführen der zugehörigen Aktion
+		if (source == gui_btn_menu) isShowMenu=!isShowMenu;
+		if (source == gui_btn_questlog) {
+			ExpMode expMode = (ExpMode)game.getState(ExpMode.ID);
+			if (!expMode.isDebug()) {
+				expMode.setDebug(true);
+			} else {
+				expMode.setDebug(false);
+			}	
+		}
+		if (source == gui_btn_back) isShowMenu=false;
+		if (source == gui_btn_save) game.enterState(MenuCredits.ID);
+		if (source == gui_btn_load) game.enterState(MenuLoad.ID);
+		if (source == gui_btn_settings) game.enterState(MenuSettings.ID);
+		if (source == gui_btn_quit) game.enterState(MenuMain.ID);
+	}
+
+}
