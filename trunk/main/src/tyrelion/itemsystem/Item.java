54ded2ab7542b65859b108d0a96f8d8a7d9b180b
daennart <daennart@2b440a80-7abf-11de-a7be-13e53affbef8>
2009-09-28 16:55:28 +0000

    Item and subitem food implemented

---
+++ trunk/main/src/tyrelion/itemsystem/Item.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package tyrelion.itemsystem;
+
+import org.newdawn.slick.Image;
+
+/**
+ * @author Basti
+ *
+ */
+public abstract class Item {
+
+	/** the unique ID of the Item */
+	private final int uid;
+	
+	/** the name of the item */
+	private final String name;
+	
+	/** the image for the item rendered in th world */
+	private final Image image_world;
+	
+	/** the image for the item rendered in the inventory */
+	private final Image image_inv;
+
+
+
+	/**
+	 * @param uID
+	 * @param name
+	 * @param imageWorld
+	 * @param imageInv
+	 */
+	public Item(int uid, String name, Image image_world, Image image_inv) {
+		super();
+		this.uid = uid;
+		this.name = name;
+		this.image_world = image_world;
+		this.image_inv = image_inv;
+	}
+	
+	/**
+	 * @return the uID
+	 */
+	public int getUid() {
+		return uid;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the image_world
+	 */
+	public Image getImage_world() {
+		return image_world;
+	}
+
+	/**
+	 * @return the image_inv
+	 */
+	public Image getImage_inv() {
+		return image_inv;
+	}
+	
+
+}
