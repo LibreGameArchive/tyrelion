8989c0dceaec92143e06e6428318b1367c453ef4
daennart <daennart@2b440a80-7abf-11de-a7be-13e53affbef8>
2009-09-28 16:55:28 +0000

    Item and subitem food implemented

---
+++ main/src/tyrelion/itemsystem/Food.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package tyrelion.itemsystem;
+
+import org.newdawn.slick.Image;
+
+/**
+ * @author imladriel
+ *
+ */
+public class Food extends Item {
+
+	/**
+	 * @param uid
+	 * @param name
+	 * @param imageWorld
+	 * @param imageInv
+	 */
+	public Food(int uid, String name, Image imageWorld, Image imageInv) {
+		super(uid, name, imageWorld, imageInv);
+	}
+
+}
